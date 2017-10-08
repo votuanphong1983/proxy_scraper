39c3d52d29975d49ddcaf9ba377f1383df07e88e
votuanphong1983 <votuanphong@gmail.com>
2017-10-08 14:11:09 +0700

    scrape from http://freeproxylists.net/

--- scraper.js
+++ scraper.js
@@ -114,18 +114,63 @@ function scrapper(db, site, type) {
 		});
 	});
 }
+function scrapperFromFreeProxyLists(db, site, type) {
+	return new Promise(function(resolve, reject) {
+		fetchPage(site, function(body) {
+			var $ = cheerio.load(body);
 
+			var elements = $("table.DataGrid > tbody > tr");
+			console.log()
+			console.log("Found " + elements.length + " elements");
+			elements.each(function() {
+				var item = $(this).find("td");
+
+				var ip = $(item[0]).text();
+				var port = parseInt($(item[1]).text(), 10);
+				var code = $(item[4]).text();
+				var country = $(item[4]).text();
+				var anonymity = $(item[3]).text();
+				var google = 'no';
+				var https = $(item[2]).text();
+
+				var d = new Date();
+				var reg = /(1 day|\d+? days)? ?(1 hour|\d+? hours)? ?(1 minute|\d+? minutes)? ?(1 second|\d+? seconds)? ago/i;
+				//var last = $(item[7]).text();
+				// if (reg.test(last)) {
+				// 	var matches = last.match(reg);
+				// 	if (matches[1] != undefined) {
+				// 		d.setDate(d.getDate() - parseInt(matches[1], 10));
+				// 	}
+				// 	if (matches[2] != undefined) {
+				// 		d.setHours(d.getHours() - parseInt(matches[2], 10));
+				// 	}
+				// 	if (matches[3] != undefined) {
+				// 		d.setMinutes(d.getMinutes() - parseInt(matches[3], 10));
+				// 	}
+				// 	if (matches[4] != undefined) {
+				// 		d.setSeconds(d.getSeconds() - parseInt(matches[4], 10));
+				// 	}
+				// }
+
+				var lastchecked = d.toJSON();
+
+				updateRow(db, ip, port, code, country, anonymity, google, https, lastchecked, type);
+			});
+			resolve();
+		});
+	});
+}
 function run(db) {
 	var scrappers = [];	
-	scrappers.push(scrapper(db, "https://free-proxy-list.net/", "free"));
-	scrappers.push(scrapper(db, "https://free-proxy-list.net/uk-proxy.html", "uk"));	
-	scrappers.push(scrapper(db, "https://free-proxy-list.net/anonymous-proxy.html", "free"));
-	scrappers.push(scrapper(db, "http://www.google-proxy.net/", "anonymous"));		
-	//skipped web proxies
-	scrappers.push(scrapper(db, "https://www.sslproxies.org/", "ssl"));
-	scrappers.push(scrapper(db, "https://us-proxy.org/", "us"));	
+	// scrappers.push(scrapper(db, "https://free-proxy-list.net/", "free"));
+	// scrappers.push(scrapper(db, "https://free-proxy-list.net/uk-proxy.html", "uk"));	
+	// scrappers.push(scrapper(db, "https://free-proxy-list.net/anonymous-proxy.html", "free"));
+	// scrappers.push(scrapper(db, "http://www.google-proxy.net/", "anonymous"));		
+	// //skipped web proxies
+	// scrappers.push(scrapper(db, "https://www.sslproxies.org/", "ssl"));
+	// scrappers.push(scrapper(db, "https://us-proxy.org/", "us"));	
 	//skipped socks4/socks5	
-	scrappers.push(scrapper(db, "https://www.socks-proxy.net/", "google"));		
+	scrappers.push(scrapperFromFreeProxyLists(db, "http://freeproxylists.net/", "free"));		
 
 	Promise.all(scrappers).then(function() {
 		cleanUp(db).then(function() {
